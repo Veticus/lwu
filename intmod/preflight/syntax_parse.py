722868c1bc3f0eccfed0df7425e96d789ec07dcd
Tim <6426821+Veticus@users.noreply.github.com>
2024-01-12 16:52:43 +0100

    Add parsing of commands in syntax
    More or less rewrote the syntax parser, because adding the command parsing revealed some edge case problems.
    Also added two tests to verify that if a command is parsed, no normal syntax is parsed.

--- intmod/preflight/syntax_parse.py
+++ intmod/preflight/syntax_parse.py
@@ -2,16 +2,49 @@ import re
 
 
 def parse_input_string(input_string):
+	"""
+	Parses the lwu input syntax.
+
+	Syntax components:
+	1: Letters [required]
+	2: Length [optional]
+	3: Letter/position pair(s) [optional]
+	4: Command [optional]
+	5: Command arguments [optional]
+
+
+	:param input_string:
+	:return: letters: A list of the letters of the input string, in alphabetical order.
+	:return: position: A list with what is assumed to be the position/letter pairs.
+	:return: length: A list with what is assumed to be the length of the desired output.
+	:return: command: If an input starts with ":" we assume a command is issued. This is the assumed command. The ":" is stripped.
+	:return: arguments: The argument belonging to (following) the command.
+	"""
 	# TODO: Syntax enforcement: Length if length should be range(1, 3)
 	# TODO: Syntax enforcement: Only one length int acceptable. If >1 length found, return "syntax error" flag for UI to deal with
-	# TODO: Syntax enforcement: If no "letters" supplied, give default or None
 	# TODO: Syntax enforcement: Length if letters should be range(1, 26)
 	# TODO: Implement "trash" parse: If something that doesn't match 'length', 'positions' or 'letters' found: return "syntax error" flag for UI to deal with
+	# TODO: Implement dev mode
 
-	letters = sorted(
-		re.findall(r'[a-zA-Z]', input_string),
-		key=lambda x: x.lower()
-	)
-	lengths = list(map(int, re.findall(r'\b(\d+)\b', input_string)))
-	positions = re.findall(r'([a-zA-Z]\d+)', input_string)
-	return letters, lengths, positions
+	# Some defaults to return quickly, in case the input syntax is a command.
+	letters = []
+	position = []
+	length = []
+	command = ""
+	command_arg = ""
+
+	if input_string.startswith(":"):  # If the input is a command
+		command_syntax = re.findall(r':\b[a-z]+\b(?: [a-z]+)?', input_string)[0]
+
+		command = command_syntax.split()[0][1:]
+		if len(command_syntax.split()) > 1:
+			command_arg = command_syntax.split()[1]
+
+	else:  # If the input is "normal" usage
+		letters = re.search(r'\b[a-zA-Z]+\b', input_string)  # Match and ignore the letterpos letter
+		letters = letters.group()  # Only get the word
+		letters = sorted(letters, key=lambda x: x.lower())  # Sort the letters
+		length = list(map(int, re.findall(r'\b(\d+)\b', input_string)))
+		position = re.findall(r'([a-zA-Z]\d+)', input_string)
+
+	return letters, length, position, command, command_arg
--- test/test_syntax_parse.py
+++ test/test_syntax_parse.py
@@ -5,35 +5,45 @@ from intmod.preflight.syntax_parse import parse_input_string
 class TestSyntaxParse(TestCase):
 	def test_input_length_multi_pospair(self):
 		input_str = "shovel 4 v4 e5"
-		expected_output = (['e', 'e', 'h', 'l', 'o', 's', 'v', 'v'], [4], ['v4', 'e5'])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [4], ['v4', 'e5'], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
 
 	def test_input_nolength_multi_pospair(self):
 		input_str = "shovel v4 e5"
-		expected_output = (['e', 'e', 'h', 'l', 'o', 's', 'v', 'v'], [], ['v4', 'e5'])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [], ['v4', 'e5'], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
 
 	def test_input_length_single_pospair(self):
 		input_str = "shovel 4 v4"
-		expected_output = (['e', 'h', 'l', 'o', 's', 'v', 'v'], [4], ['v4'])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [4], ['v4'], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
 
 	def test_input_nolength_single_pospair(self):
 		input_str = "shovel e5"
-		expected_output = (['e', 'e', 'h', 'l', 'o', 's', 'v'], [], ['e5'])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [], ['e5'], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
 
 	def test_input_length_no_pospair(self):
 		input_str = "shovel 4"
-		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [4], [])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [4], [], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
 
 	def test_input_nolength_no_pospair(self):
 		input_str = "shovel"
-		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [], [])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [], [], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
 
 	def test_input_length_multi_pospair_out_of_order(self):
 		input_str = "4 v4 shovel"
-		expected_output = (['e', 'h', 'l', 'o', 's', 'v', 'v'], [4], ['v4'])
-		self.assertEqual(parse_input_string(input_str), expected_output)
+		expected_output = (['e', 'h', 'l', 'o', 's', 'v'], [4], ['v4'], "", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
+
+	def test_command_noarg(self):
+		input_str = ":command"
+		expected_output = ([], [], [], "command", "")
+		self.assertEqual(expected_output, parse_input_string(input_str))
+
+	def test_command_arg(self):
+		input_str = ":command argument"
+		expected_output = ([], [], [], "command", "argument")
+		self.assertEqual(expected_output, parse_input_string(input_str))
